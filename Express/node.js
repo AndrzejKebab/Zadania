87a1aad94ece2603daeac278b981174fbde1dcb9
Andrzej Kebab <73285275+AndrzejKebab@users.noreply.github.com>
2021-09-13 15:09:55 +0200

    saada

--- Express/node.js
+++ Express/node.js
@@ -10,15 +10,5 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
 
-module.exports = {
-    area: function(width) {
-      return width * width;
-    },
-  
-    perimeter: function(width) {
-      return 4 * width;
-    }
-  };
-
-    console.log('First');
-    console.log('Second');
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
