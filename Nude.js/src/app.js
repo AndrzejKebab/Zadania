b7d89fa85c9d7e531e31fd74fb0e2ad9ac06f631
Andrzej Kebab <73285275+AndrzejKebab@users.noreply.github.com>
2021-09-20 15:22:30 +0200

    sss

--- Nude.js/src/app.js
+++ Nude.js/src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('views', './src/views');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send("Strona Działa! POG");
+    //res.send("Strona Działa! POG");
+    res.render('index', {});
 });
 
 app.get('/omnie', (req, res) => {
